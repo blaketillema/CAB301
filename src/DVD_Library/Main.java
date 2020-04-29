f3ef64e17c4a3ce465e51f156bda0f88dc132663
blaketillema <61172664+blaketillema@users.noreply.github.com>
2020-04-29 13:56:08 +1000

    menu stuff idk

--- src/DVD_Library/Main.java
+++ src/DVD_Library/Main.java
@@ -1,20 +1,40 @@
 package DVD_Library;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String args[]){
 
-        String[] movies = {"b", "q", "a", "s", "p", "g", "f"};
-        MovieCollection mvc = new MovieCollection();
-        for(int i = 0; i < movies.length; i++){
-            mvc.addMovie(movies[i], "info", 10);
+        Scanner scanner = new Scanner(System.in);
+        String in = "";
+        boolean movie = false;
+        boolean user = false;
+
+        while(!in.equals("0")){
+
+            if(in.equals("1")){
+                user = true;
+                movie = false;
+            }
+            else if(in.equals("2")){
+                movie = true;
+                user = false;
+            }
+            else if(in.equals("3")){
+                movie = false;
+                user = false;
+                in = "";
+            }
+            else{
+                System.out.println("Select an option [1 - 2]\n 1. user menu\n 2. movie menu\n\n 0. exit");
+                System.out.print("Select: ");
+                in = scanner.nextLine();
+            }
+            if(user){
+                System.out.println("-----User Menu-----");
+                in = scanner.nextLine();
+            }
         }
-
-        MemberCollection mbc = new MemberCollection();
-        mbc.addMember("jef");
-        mbc.addMember("manem");
-        mbc.getMembers();
-
     }
-
 }
