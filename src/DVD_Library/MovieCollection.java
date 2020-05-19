14d15de7b89fe6149b1dfd9a809e8ef34b8c2510
Blake <blaketillema@gmail.com>
2020-05-19 18:17:02 +1000

    search fixed? finish top 10

--- src/DVD_Library/MovieCollection.java
+++ src/DVD_Library/MovieCollection.java
@@ -28,21 +28,13 @@ public class MovieCollection {
     }
 
     public Movie search(Movie root, String title){
-        if(root == null){
-            return null;
-        }
-        else if(root.title.equals(title)){
+        if(root == null || root.title.compareTo(title) == 0){
             return root;
         }
-        else{
-            if(root.title.compareTo(title) < 0){
-                search(root.left, title);
-            }
-            else{
-                search(root.right, title);
-            }
+        if(root.title.compareTo(title) > 0){
+            return search(root.left, title);
         }
-        return root;
+        return search(root.right, title);
     }
 
     public void removeMovie(String title){
@@ -126,7 +118,7 @@ public class MovieCollection {
             root.copiesAvailable--;
         }
         else{
-            if(root.title.compareTo(m.title) < 0){
+            if(m.title.compareTo(root.title) < 0){
                 borrowMovieRecur(root.left, m);
             }
             else{
@@ -144,7 +136,7 @@ public class MovieCollection {
             root.copiesAvailable++;
         }
         else{
-            if(root.title.compareTo(m.title) < 0){
+            if(m.title.compareTo(root.title) < 0){
                 returnMovieRecur(root.left, m);
             }
             else{
@@ -170,9 +162,49 @@ public class MovieCollection {
         int len = length(root);
         Movie[] movies = new Movie[len];
         movies = toArray(root, movies);
-        for(int i = 0; i < len; i++){
-            System.out.println(movies[i].title);
+        movies = heapsort(movies);
+        Movie[] topTen = new Movie[10];
+        for(int i = 0; i < 10; i++){
+            topTen[i] = movies[movies.length - i - 1];
+            System.out.println(topTen[i].title + " - " + topTen[i].timesBorrowed);
         }
+    }
+
+    private Movie[] heapsort(Movie[] movies){
+        int n = movies.length;
+
+        for(int i = n / 2 - 1; i >=0; i--){
+            movies = heapify(movies, n, i);
+        }
+
+        for(int i = n - 1; i > 0; i--){
+            Movie temp = movies[0];
+            movies[0] = movies[i];
+            movies[i] = temp;
+            movies = heapify(movies, i, 0);
+        }
+        return movies;
+    }
+
+    private Movie[] heapify(Movie[] movies, int n, int i){
+        int mostPopular = i;
+        int l = 2*i;
+        int r = 2*i + 1;
+
+        if(l < n && movies[l].timesBorrowed > movies[mostPopular].timesBorrowed){
+            mostPopular = l;
+        }
+        if(r < n && movies[r].timesBorrowed > movies[mostPopular].timesBorrowed){
+            mostPopular = r;
+        }
+        if(mostPopular != i){
+            Movie swap = movies[i];
+            movies[i] = movies[mostPopular];
+            movies[mostPopular] = swap;
+
+            movies = heapify(movies, n, mostPopular);
+        }
+        return movies;
     }
 
     private Movie[] toArray(Movie movie, Movie[] movies){
@@ -194,17 +226,9 @@ public class MovieCollection {
             System.out.println("Title: " + movie.title);
             System.out.println("Description: " + movie.info);
             System.out.println("Copies available: " + movie.copiesAvailable);
+            System.out.println("Times borrowed: " + movie.timesBorrowed);
             System.out.println();
             inOrderTraversal(movie.right);
-        }
-    }
-
-    private void inReverseOrderTraversal(Movie movie){
-        if(movie != null){
-            inReverseOrderTraversal(movie.right);
-            System.out.println("Title: " + movie.title);
-            System.out.println("Times borrowed: " + movie.timesBorrowed);
-            inReverseOrderTraversal(movie.left);
         }
     }
 }
